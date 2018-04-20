4f48ac484098c68b6259a87c6a15860e53aa922a
Rachel Greaner <rgreaner8@gmail.com>
2018-04-20 13:29:27 -0500

    Update on user model

--- models/user.js
+++ models/user.js
@@ -35,7 +35,7 @@ module.exports = function (sequelize, DataTypes) {
         });
     }
     User.associate = function (models) {
-        User.hasMany(models.Categories, {
+        User.hasMany(models.Category, {
             onDelete: "cascade"
         });
     };
